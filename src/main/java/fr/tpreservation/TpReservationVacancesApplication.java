db0d89863b04589ee0dcab42e3f6967412628a38
Faxiis <antho.lavoute@gmail.com>
2024-10-03 09:40:11 +0200

    Initial commit

---
+++ src/main/java/fr/tpreservation/TpReservationVacancesApplication.java
@@ -0,0 +1,13 @@
+package fr.tpreservation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TpReservationVacancesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TpReservationVacancesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/fr/tpreservation/TpReservationVacancesApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.tpreservation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TpReservationVacancesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
